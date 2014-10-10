e965a502601f63d62eeb880dc183e4b57990acd2
Mauricio D. De la Barra <mauridelabarra@gmail.com>
2014-10-10 18:38:10 -0400

    Login and Register processing
    Controller files for processing Logging In and Registration

---
+++ app/controller/processLogin.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once '../global.php';
+
+$username = $_POST['username'];
+$password = $_POST['pass'];
+
+// get user with this username from database
+$user = User::loadByUsername($username);
+
+// if user exists, username is valid
+if($user == null) {
+    $_SESSION['error'] = "Invalid username.";
+    header('Location: '.SERVER_PATH.'login');
+    exit();
+} else {
+    // does inputted password match password in db?
+    if($user->get('password') != $password) {
+        $_SESSION['error'] = "Invalid password.";
+        header('Location: '.SERVER_PATH.'login'); 
+        exit();
+    } else {
+        $_SESSION['user'] = $username;
+
+        $_SESSION['id'] = $user->get('id');
+        $_SESSION['firstName'] = $user->get('firstName');
+        $_SESSION['lastName'] = $user->get('lastName');
+        $_SESSION['description'] = $user->get('description');
+        $_SESSION['password'] = $user->get('password');
+        $_SESSION['image'] = $user->get('image');
+        $_SESSION['gender'] = $user->get('gender');
+        $_SESSION['topic_id'] = $user->get('topic_id');
+        $_SESSION['topic_id1'] = $user->get('topic_id1');
+
+        header('Location: '.SERVER_PATH.'app/view/success.php');
+        exit();
+    }
+}
---
+++ app/controller/register.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../global.php';
+
+$pageName = 'Register';
+
+
+require_once '../view/register.tpl';
